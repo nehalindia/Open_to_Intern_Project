6b129d9c9845aca5fac7256681ec07e5db1ee322
kazmy <nehaluddin.dpe@gmail.com>
2023-06-08 22:31:55 +0530

    added post api

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,6 +1,76 @@
+const validurl = require('valid-url')
+const Obj = require('mongoose').Types.ObjectId
 const clgModel = require('../model/collegeModel')
 const intModel = require('../model/internModel')
 
+const createClg = async (req,res) => {
+   try{
+        let data = req.body
+        if(!data.name){
+            return res.status(400).send({status :false, message: "Must add name"})
+        }
+        let name = await clgModel.findOne({name:data.name})
+        if(name){
+            return res.status(400).send({status :false, message: " name already exist!"})
+        }
+        if(!data.fullName){
+            return res.status(400).send({status :false, message: "Must add fullname"})
+        }
+        if (!validurl.isUri(data.logoLink)){
+            return res.status(400).send({status :false, message: "Not a valid Url"})
+        }
+
+        let result = await clgModel.create(data)
+        let responseData = {name:result.name, fullName:result.fullName, logoLink:result.logoLink, isDeleted:result.isDeleted }
+        return res.status(201).send({status:true, data: responseData})
+    }catch(error){
+        res.status(500).send({status : false,message: error.message})
+    }
+}
 
 
-module.exports = {}
+const createIntern = async (req,res) => {
+    try{
+        let data = req.body
+        if(!data.name){
+            return res.status(400).send({status :false, message: "Must add name"})
+        }
+        if(!data.email){
+            return res.status(400).send({status :false, message: "Must add name"})
+        }
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(data.email)) {
+            return res.status(400).send({status: false, message:  'Email should be a valid email address'})
+        }
+        const existingAuthor = await intModel.findOne({ email: data.email });
+        if(existingAuthor) {
+                return res.status(400).send({status: false, message:  'Email already exists'});
+        }
+        if(!data.mobile){
+            return res.status(400).send({status :false, message: "Must add mobile number"})
+        }
+        if(data.mobile.length !== 10){
+            return res.status(400).send({status :false, message: "Must add valid mobile number"})
+        }
+        if(!data.collegeName){
+            return res.status(400).send({status :false, message: "Must add clg"})
+        }
+        // if(!obj.isValid(data.collegeId)){
+        //     return res.status(400).send({status :false, message: "Not a valid Id"})
+        // }
+        let clgId = await clgModel.findOne({name:data.collegeName})
+        if(!clgId) {
+            return res.status(400).send({status: false, message:  'College not exists'});
+        }
+        data.collegeId = clgId._id
+        delete data.collegeName
+
+        let result = await intModel.create(data)
+        let responseData = {isDeleted:result.isDeleted, name: result.name, email: result.email, 
+            mobile:result.mobile, collegeId: result.collegeId}
+        return res.status(201).send({status:true, data: responseData})
+    }catch(error){
+        res.status(500).send({status : false,message: error.message})
+    }
+}
+
+module.exports = {createClg, createIntern}
--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const {} = require('../controller/internController')
+const {createClg, createIntern} = require('../controller/internController')
 
-router.post('/functionup/colleges',)
-router.post('/functionup/interns',)
+router.post('/functionup/colleges',createClg)
+router.post('/functionup/interns',createIntern)
 
 router.get('/functionup/collegeDetails',)
 
